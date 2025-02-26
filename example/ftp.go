0312194711507819fd4c53591585042e100a949e
labulakalia <labulakalia@gmail.com>
2025-02-26 12:15:47 +0000

    fix ftps

--- example/ftp.go
+++ example/ftp.go
@@ -3,12 +3,13 @@ package main
 import (
 	"crypto/tls"
 	"log"
+	"log/slog"
 	"net"
+	"os"
 	"time"
 
 	"github.com/jlaffaye/ftp"
-	wasi_net "github.com/labulakalia/wazero_net/wasi/net"
-	// wasi_net "github.com/labulakalia/wazero_net/wasi/net"
+	wazero_net "github.com/labulakalia/wazero_net/wasi/net"
 )
 
 type Conn struct {
@@ -37,25 +38,26 @@ func main() {
 
 //go:wasmexport ftp_connect
 func ftp_connect() {
-	ftpConn, err := ftp.Dial("127.0.0.1:21", ftp.DialWithDialFunc(func(network, address string) (net.Conn, error) {
-		conn, err := wasi_net.Dial(network, address)
-		if err != nil {
-			return nil, err
-		}
-		return conn,err
-		// return NewConn(conn, time.Second), nil
-	}),
-		ftp.DialWithExplicitTLS(&tls.Config{InsecureSkipVerify: true}),
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	ftp_conn()
+	ftps()
+	ftps_implicit()
+}
+
+func ftp_conn() {
+	slog.Info("ftp connect")
+	ftpConn, err := ftp.Dial("127.0.0.1:2121",
+		ftp.DialWithDialFunc(func(network, address string) (net.Conn, error) {
+			return wazero_net.Dial(network, address)
+		}),
+		ftp.DialWithDebugOutput(os.Stdout),
 	)
+
 	if err != nil {
 		log.Panicln(err)
 	}
 	user := "user"
 	password := "passwd"
-	if user == "" && password == "" {
-		password = "anonymous"
-		user = "anonymous"
-	}
 	err = ftpConn.Login(user, password)
 	if err != nil {
 		log.Panicln(err)
@@ -65,5 +67,64 @@ func ftp_connect() {
 		log.Panicln(err)
 	}
 
-	log.Printf("%+v\n", entries)
+	log.Printf("ftps %+v\n", entries)
+}
+
+func ftps() {
+	slog.Info("ftps connect")
+	ftpConn, err := ftp.Dial("127.0.0.1:21",
+		ftp.DialWithDialFunc(func(network, address string) (net.Conn, error) {
+			return wazero_net.Dial(network, address)
+		}),
+		ftp.DialWithExplicitTLS(&tls.Config{InsecureSkipVerify: true}),
+		ftp.DialWithDebugOutput(os.Stdout),
+	)
+
+	if err != nil {
+		log.Panicln(err)
+	}
+	user := "user"
+	password := "passwd"
+	err = ftpConn.Login(user, password)
+	if err != nil {
+		log.Panicln(err)
+	}
+	entries, err := ftpConn.List("/")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Printf("ftps %+v\n", entries)
+}
+
+func ftps_implicit() {
+	slog.Info("ftps_implicit connect")
+	ftpConn, err := ftp.Dial("127.0.0.1:990",
+		ftp.DialWithDialFunc(func(network, address string) (net.Conn, error) {
+			conn, err := wazero_net.Dial(network, address)
+			if err != nil {
+				return nil, err
+			}
+			return tls.Client(conn, &tls.Config{InsecureSkipVerify: true}), nil
+		}),
+		ftp.DialWithTLS(&tls.Config{InsecureSkipVerify: true}),
+		ftp.DialWithDebugOutput(os.Stdout),
+	)
+
+	if err != nil {
+		log.Panicln(err)
+	}
+	user := "user"
+	password := "passwd"
+	err = ftpConn.Login(user, password)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("ftp conn list")
+	entries, err := ftpConn.List("/")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Printf("ftps_implicit %+v\n", entries)
 }
--- example/ftps.go
+++ example/ftps.go
@@ -3,23 +3,31 @@ package main
 import (
 	"crypto/tls"
 	"log"
+	"net"
+	"os"
 
 	"github.com/jlaffaye/ftp"
 )
 
 func main() {
-	ftpConn, err := ftp.Dial("127.0.0.1:990",
-		ftp.DialWithTLS(&tls.Config{InsecureSkipVerify: true}),
+	ftps()
+	// ftps_implicit()
+}
+
+func ftps() {
+	ftpConn, err := ftp.Dial("127.0.0.1:2121",
+		// ftp.DialWithDialFunc(func(network, address string) (net.Conn, error) {
+		// 	return net.Dial(network, address)
+		// }),
+		// ftp.DialWithExplicitTLS(&tls.Config{InsecureSkipVerify: true}),
+		ftp.DialWithDebugOutput(os.Stdout),
 	)
+
 	if err != nil {
 		log.Panicln(err)
 	}
 	user := "user"
 	password := "passwd"
-	if user == "" && password == "" {
-		password = "anonymous"
-		user = "anonymous"
-	}
 	err = ftpConn.Login(user, password)
 	if err != nil {
 		log.Panicln(err)
@@ -29,5 +37,36 @@ func main() {
 		log.Panicln(err)
 	}
 
-	log.Printf("%+v\n", entries)
+	log.Printf("ftps %+v\n", entries)
+}
+
+func ftps_implicit() {
+	ftpConn, err := ftp.Dial("127.0.0.1:990",
+		ftp.DialWithDialFunc(func(network, address string) (net.Conn, error) {
+			conn, err := net.Dial(network, address)
+			if err != nil {
+				return nil, err
+			}
+			return tls.Client(conn, &tls.Config{InsecureSkipVerify: true}), nil
+		}),
+		ftp.DialWithTLS(&tls.Config{InsecureSkipVerify: true}),
+		ftp.DialWithDebugOutput(os.Stdout),
+	)
+
+	if err != nil {
+		log.Panicln(err)
+	}
+	user := "ftpuser"
+	password := "admin"
+	err = ftpConn.Login(user, password)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("ftp conn list")
+	entries, err := ftpConn.List("/")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Printf("ftps_implicit %+v\n", entries)
 }
