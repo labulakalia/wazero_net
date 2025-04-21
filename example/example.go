d6f8592da90df713de802ccbb1e85ea447e8bbb1
labulakalia <labulakalia@gmail.com>
2025-04-21 09:54:50 +0800

    change README.md

--- example/example.go
+++ example/example.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"crypto/rand"
 	_ "embed"
-	"fmt"
 	"log"
 	"log/slog"
 	"os"
-	"runtime"
 
 	"github.com/labulakalia/wazero_net"
 	"github.com/tetratelabs/wazero"
@@ -16,11 +14,10 @@ import (
 )
 
 func main() {
-
 	ctx := context.Background()
 
-	// add wasm cache
-	cacheDir := "cache"
+	// // add wasm cache
+	cacheDir := os.TempDir()
 	os.MkdirAll(cacheDir, 0755)
 
 	cache, err := wazero.NewCompilationCacheWithDir(cacheDir)
@@ -29,21 +26,20 @@ func main() {
 	}
 	rConfig := wazero.NewRuntimeConfig().
 		WithCompilationCache(cache)
-	readMem()
+
 	r := wazero.NewRuntimeWithConfig(ctx, rConfig)
-	defer r.Close(ctx)
+	// defer r.Close(ctx)
+	// r := wazero.NewRuntime(ctx)
+	defer r.Close(ctx) //
 	_, err = wazero_net.InitFuncExport(r).Instantiate(ctx)
 	if err != nil {
 		slog.Error("Instantiate failed", "err", err)
 		return
 	}
 
-	defer cache.Close(ctx)
-
 	wasi_snapshot_preview1.MustInstantiate(ctx, r)
 
-	conf := wazero.NewModuleConfig().
-		WithStartFunctions("_initialize").
+	conf := wazero.NewModuleConfig().WithStartFunctions("_initialize").
 		WithStdout(os.Stdout).
 		WithStderr(os.Stderr).
 		WithStdin(os.Stdin).
@@ -53,6 +49,7 @@ func main() {
 		WithSysWalltime()
 
 	if os.Args[1] == "net" {
+
 		netWasm, err := os.ReadFile("net.wasm")
 		if err != nil {
 			log.Panicln(err)
@@ -72,27 +69,29 @@ func main() {
 		if err != nil {
 			log.Panicln(err)
 		}
-		readMem()
+
 		cm, err := r.CompileModule(context.Background(), netWasm)
 		if err != nil {
 			log.Panicln(err)
 		}
-		readMem()
 
 		httpMod, err := r.InstantiateModule(ctx, cm, conf)
 		if err != nil {
 			log.Panicln(err)
 		}
-		readMem()
-		malloc := httpMod.ExportedFunction("malloc")
-		url := "https://httpbin.org/get"
-		result, err := malloc.Call(ctx, uint64(len(url)))
-		if err != nil {
-			log.Fatalln("malloc", err)
-		}
-		httpMod.Memory().Write(uint32(result[0]), []byte(url))
+		https := httpMod.ExportedFunction("https_get")
 
-		_, err = httpMod.ExportedFunction("https_get").Call(ctx, result[0], uint64(len(url)))
+		malloc := httpMod.ExportedFunction("malloc")
+		free := httpMod.ExportedFunction("free")
+		url := "https://httpbin.org/get"
+		results, err := malloc.Call(ctx, uint64(len(url)))
+		if err != nil {
+			log.Panicln(err)
+		}
+		defer free.Call(ctx, results[0])
+		httpMod.Memory().Write(uint32(results[0]), []byte(url))
+
+		_, err = https.Call(ctx, results[0], uint64(len(url)))
 		if err != nil {
 			log.Fatalln("https get", err)
 		}
@@ -140,26 +139,8 @@ func main() {
 		if err != nil {
 			log.Panicln(err)
 		}
-
-		mod.ExportedFunction("smb_connect").Call(context.Background())
+		mod.ExportedFunction("smb_connect").Call(ctx)
+		slog.Info("exit smb")
+		// exit wait 60 second
 	}
-	r.Close(context.Background())
-	// runtime.GC()
-	readMem()
-}
-func readMem() {
-	slog.SetLogLoggerLevel(slog.LevelDebug)
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-
-	// fmt.Println(ms) // 太复杂了
-
-	// 内存 通义千问
-	// 打印一些关键的内存统计信息
-	slog.Debug(fmt.Sprintf("Mem Usage Alloc = %v MiB", m.Alloc/1024/1024))           // 分配但未释放的内存
-	slog.Debug(fmt.Sprintf("Mem Usage TotalAlloc = %v MiB", m.TotalAlloc/1024/1024)) // 程序启动以来分配的总内存
-	slog.Debug(fmt.Sprintf("Mem Usage Sys = %v MiB", m.Sys/1024/1024))               // 从操作系统分配的总内存
-	slog.Debug(fmt.Sprintf("Mem Usage HeapAlloc = %v MiB", m.HeapAlloc/1024/1024))   // 从堆上分配但未释放的内存
-	slog.Debug(fmt.Sprintf("Mem Usage HeapSys = %v MiB", m.HeapSys/1024/1024))       // 由Go分配的堆内存的系统内存大小
-	slog.Debug(fmt.Sprintf("Mem Usage NumGC = %v", m.NumGC))                         // 进行的GC次数
 }
--- go.mod
+++ go.mod
@@ -3,25 +3,6 @@ module github.com/labulakalia/wazero_net
 go 1.24.0
 
 require (
-	github.com/cloudsoda/go-smb2 v0.0.0-20250206185559-eb0657583727
-	github.com/jlaffaye/ftp v0.2.0
-	github.com/pkg/sftp/v2 v2.0.0-20250222081549-c244e3d6ad0d
 	github.com/tetratelabs/wazero v1.9.0
-	golang.org/x/crypto v0.34.0
-)
-
-require (
-	github.com/cloudsoda/sddl v0.0.0-20250203211958-38b2ba66781c // indirect
-	github.com/geoffgarside/ber v1.1.0 // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/hashicorp/go-uuid v1.0.3 // indirect
-	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
-	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
-	github.com/jcmturner/gofork v1.7.6 // indirect
-	github.com/jcmturner/goidentity/v6 v6.0.1 // indirect
-	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
-	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	tinygo.org/x/drivers v0.31.0
 )
